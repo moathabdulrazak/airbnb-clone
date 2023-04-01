01db7745e287e9da9a2a958c4f76159384979cd3
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-01 09:23:30 -0600

    modal functionlaity

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal/>
+          <Modal title='hello' isOpen />
         <Navbar/>
         </ClientOnly>
         {children}</body>
