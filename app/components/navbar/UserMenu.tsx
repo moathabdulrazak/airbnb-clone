1579f096e905a64f6bba142f543047d1fa22fffa
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-01 08:11:15 -0600

    login option under menu added

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,9 +1,15 @@
 'use client'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import {AiOutlineMenu} from 'react-icons/ai'
 import Avatar from '../Avatar'
+import MenuItem from './MenuItem'
 
 export const UserMenu = () => {
+  const[isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback (() => {
+    setIsOpen((value) => !value);
+  }, [])
   return (
     <div className='relative' >
       <div className='flex flex-row items-center gap-3' >
@@ -25,7 +31,7 @@ export const UserMenu = () => {
           Airbnb your home
         </div>
         <div 
-        onClick={() => {}}
+        onClick={toggleOpen}
         className='
         p-4
         md:py-1
@@ -47,6 +53,32 @@ export const UserMenu = () => {
           </div>
         </div>
       </div>
+
+
+      {isOpen && (
+        <div className='
+        absolute
+        rounded-xl
+        shadow-md
+        w-[40vw]
+        md:w-3/4
+        bg-white
+        overflow-hidden
+        right-0
+        top-12
+        text-sm
+        ' 
+        >
+          <div className='flex flex-col cursor-pointer' >
+          <>
+          <MenuItem
+          onClick={() => {}}
+          label='Login'
+          />
+          </>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
