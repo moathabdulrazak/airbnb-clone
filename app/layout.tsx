4648a2a3e21beacabe2f714035b23fadb2c1e714
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-01 08:23:50 -0600

    Modal interface setups

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 import { Navbar } from './components/navbar/Navbar'
 import './globals.css'
 import {Nunito} from "next/font/google"
@@ -20,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ClientOnly>
+          <Modal/>
         <Navbar/>
+        </ClientOnly>
         {children}</body>
     </html>
   )
