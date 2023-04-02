88216e96831c7da99c4049aa7287e5961ffcec18
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-02 07:42:28 -0600

    category compoent in the works

---
+++ app/components/inputs/CategoryInput.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+const CategoryInput = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CategoryInput
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -3,6 +3,9 @@
 import React, { useMemo, useState } from 'react'
 import Modal from './Modal'
 import useRentModal from '@/app/hooks/useRentModal'
+import Heading from '../Heading'
+import { categories } from '../navbar/Categories'
+import CategoryInput from '../inputs/CategoryInput'
 
 enum STEPS {
   CATEGORY = 0,
@@ -42,15 +45,45 @@ const RentModal = () => {
     return 'Back';
   }, [step]);
 
-  
+  let bodyContent = (
+    <div className='flex flex-col gap-8' >
+<Heading
+title='Which of these best describes your place?'
+subtile='Pick a category'
+/>
+<div className='
+grid
+grid-cols-1
+md:grid-cols-2
+gap-3
+max-h-[50vh]
+overflow-y-a
+' >
+  {categories.map((item) => (
+    <div key={item.label} className='col-span-1' >
+       <CategoryInput
+       onClick={() => {}}
+       selected={false}
+       label={item.label}
+       icon={item.icon} 
+       />
+    </div>
+  ))}
+</div>
+    </div>
+  )
+
+
   return (
     <Modal
     isOpen={rentModal.isOpen}
     onClose={rentModal.onClose}
     onSubmit={rentModal.onClose}
-    actionLabel='Submit'
+    actionLabel={actionLabel}
+    secondaryActionLabel={secondaryActionLabel}
+    secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
     title='Airbnb your home'
-    
+    body={bodyContent}
     />
   )
 }
