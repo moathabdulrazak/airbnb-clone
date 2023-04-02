cc32646cfdacb3983b0ce0974b112e1c7b85e47c
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-02 08:02:20 -0600

    selecting category functionality completed

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,6 +6,7 @@ import useRentModal from '@/app/hooks/useRentModal'
 import Heading from '../Heading'
 import { categories } from '../navbar/Categories'
 import CategoryInput from '../inputs/CategoryInput'
+import {  FieldValues, useForm } from 'react-hook-form'
 
 enum STEPS {
   CATEGORY = 0,
@@ -20,6 +21,39 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const { 
+    register, 
+    handleSubmit,
+    setValue,
+    watch,
+    formState: {
+      errors,
+    },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: '',
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: '',
+      price: 1,
+      title: '',
+      description: '',
+    }
+  });
+
+  const category = watch('category')
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true
+    })
+  }
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -62,8 +96,8 @@ overflow-y-a
   {categories.map((item) => (
     <div key={item.label} className='col-span-1' >
        <CategoryInput
-       onClick={() => {}}
-       selected={false}
+       onClick={(category) => setCustomValue('category', category)}
+       selected={category === item.label}
        label={item.label}
        icon={item.icon} 
        />
