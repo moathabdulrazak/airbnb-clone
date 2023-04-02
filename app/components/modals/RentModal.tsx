02b35b3d4b0cd7cf5f78eea0554683cfcdc06435
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-02 07:22:29 -0600

    setting up enum for rent modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,11 +1,23 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from './Modal'
 import useRentModal from '@/app/hooks/useRentModal'
 
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5
+}
+
 const RentModal = () => {
   const rentModal = useRentModal();
+
+  const [StereoPannerNode, setStep] = useState(STEPS.CATEGORY)
+
   return (
     <Modal
     isOpen={rentModal.isOpen}
