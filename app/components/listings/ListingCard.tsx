5799e950d420fffb708afa6790f4a83b0051c419
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-03 08:24:00 -0600

    listing card works

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image'
 import React, { useCallback, useMemo } from 'react'
 import {format} from 'date-fns'
 import HeartButton from '../HeartButton'
+import Button from '../Button'
 interface ListingCardProps{
   data: Listing
   reservation?: Reservation
@@ -88,6 +89,30 @@ const ListingCard: React.FC<ListingCardProps> = ({
             />
           </div>
         </div>
+            <div className='font-semibold text-lg' >
+              {location?.region}, {location?.label}
+              </div>
+              <div className='font-light text-neutral-500' >
+                {reservationDate || data.category }
+              </div>
+              <div className='flex flex-row items-center gap-1' >
+                <div className='font-semibold' >
+                $ {price}
+                </div>
+                {!reservation && (
+                  <div className='font-light'>night</div>
+                )}
+              </div>
+              <div>
+                {onAction && actionLabel && (
+                  <Button
+                  disabled={disabled}
+                  small
+                  label={actionLabel}
+                  onClick={handleCancel}
+                  />
+                )}
+              </div>
         </div>
 
     </div>
