18ba434584026e2f4b1cf027ae3f1b5c4fbf0e8b
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-03 04:11:37 -0600

    description step compelete

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from '../inputs/CountrySelect'
 import dynamic from 'next/dynamic'
 import Counter from '../inputs/Counter'
 import ImageUpload from '../inputs/ImageUpload'
+import Input from '../inputs/input'
 
 enum STEPS {
   CATEGORY = 0,
@@ -25,7 +26,8 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
-
+  const [isLoading, setIsLoading ] = useState(false)
+ 
   const { 
     register, 
     handleSubmit,
@@ -181,6 +183,34 @@ overflow-y-auto
         <ImageUpload
           onChange={(value) => setCustomValue('imageSrc', value)}
           value={imageSrc}
+        />
+      </div>
+    )
+  }
+
+  if(step === STEPS.DESCRIPTION){
+    bodyContent = (
+      <div className="flex flex-col gap-8" >
+        <Heading
+        title="How would you describe your place?"
+        subtile='Short and sweet works best!'
+        />
+        <Input
+        id='title'
+        label='Title'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        />
+        <hr />
+        <Input
+        id='description'
+        label='Description'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
         />
       </div>
     )
