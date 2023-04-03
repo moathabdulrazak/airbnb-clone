fbc23d8787f19d0930179f532a22ba6005ddef48
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-03 11:09:55 -0600

    categorys dispalying in id page.

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -37,15 +37,15 @@ const ListingClient: React.FC<ListingClientProps> = ({
               currentUser={currentUser}
               />
               <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6 ' >
-                <ListingInfo
-                user={listing.user}
-                category={category}
-                description={listing.description}
-                roomCount={listing.roomCount}
-                guestCount={listing.guestCount}
-                bathroomCount={listing.bathroomCount}
-                location={listing.locationValue}
-                />
+              <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
               </div>
             </div>
         </div>
