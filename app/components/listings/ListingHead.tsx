9ce05b178fd87b729bcae609c027ae43025f8e70
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-03 10:45:56 -0600

    reusable favoriting component works in listing page

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -4,7 +4,8 @@ import useCountries from '@/app/hooks/useCountries';
 import { SafeUser } from '@/app/types';
 import React from 'react'
 import Heading from '../Heading';
-
+import Image from 'next/image';
+import HeartButton from '../HeartButton';
 interface ListingHeadProps{
   title: string;
   locationValue: string;
@@ -34,7 +35,21 @@ const ListingHead: React.FC<ListingHeadProps> = ({
     title={title}
     subtile={`${location?.region}, ${location?.label}`}
     />
-    
+    <div className='w-full h-[60vh] overflow-hidden rounded-xl relative' >
+    <Image
+    alt='Image'
+    src={imageSrc}
+    fill
+    className='object-cover w-full'
+    />
+    <div className='absolute top-5 right-5' >
+      <HeartButton
+      listingId={id}
+      currentUser={currentUser}
+      />
+    </div>
+
+    </div>
     
     </>
   )
