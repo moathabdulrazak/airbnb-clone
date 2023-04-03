18f1a4de55f501e0408257c9921adfc26dcc704c
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-03 07:08:13 -0600

    starting on getting data

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,10 @@ export default async function RootLayout({
         <LoginModal/>
         <RegisterModal/>
         </ClientOnly>
-        {children}</body>
+        <div className='pb-20 pt-28' >
+        {children}
+        </div>
+        </body>
     </html>
   )
 }
