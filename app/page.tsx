9fc85d57f04596e821900939f82aff1bd584f9cd
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-03 07:45:44 -0600

    handle cancel function

--- app/page.tsx
+++ app/page.tsx
@@ -35,6 +35,7 @@ export default async function Home() {
           {listings.map((listing: any) => {
             return (
               <ListingCard
+              currentUser={currentUser}
               key={listing.id}
               data={listing}
               />
