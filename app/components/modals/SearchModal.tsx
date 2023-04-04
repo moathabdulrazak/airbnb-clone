8b9a9e8b65dc046a83eba8ff6724f2caedc22ccc
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 10:28:22 -0600

    starting on search

---
+++ app/components/modals/SearchModal.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Modal from './Modal'
+import useSearchModal from '@/app/hooks/useSearchModal'
+
+const SearchModal = () => {
+  const searchModal = useSearchModal();
+  return (
+    <Modal
+    
+    />
+  )
+}
+
+export default SearchModal
