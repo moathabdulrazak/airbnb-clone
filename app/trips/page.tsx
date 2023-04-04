77410f50f3cfea29425dcf208a425b289349ddd7
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 07:05:25 -0600

    setting up trips page.

---
+++ app/trips/page.tsx
@@ -0,0 +1,11 @@
+
+
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+
+const TripsPage = async () => {
+  
+}
