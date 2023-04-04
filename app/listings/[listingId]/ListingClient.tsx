c8474a3ef2cef7081126cd01704a69893bc3e3af
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 04:30:38 -0600

    you can reserve!

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -61,36 +61,38 @@ const ListingClient: React.FC<ListingClientProps> = ({
 
 
   const onCreateReservation = useCallback(() => {
-    if(!currentUser){
-      return loginModal.onOpen();
-    }
-    setIsLoading(true);
-    axios.post('/api/reservations', {
-      totalPrice,
-      startDate: dateRange.startDate,
-      endDate: dateRange.endDate,
-      listingId: listing?.id
-    })
-    .then(() => {
-      toast.success('Listing reserved!')
-      setDateRange(initialDateRange)
-      // redirect to /trips
-      router.refresh();
-    })
-    .catch(() => {
-      toast.error('Something went wrong!')
-    })
-    .finally(() => {
-      setIsLoading(false);
-    })
-  }, [
-totalPrice,
-dateRange,
-listing?.id,
-router,
-currentUser,
-loginModal
-  ])
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+      setIsLoading(true);
+
+      axios.post('/api/reservations', {
+        totalPrice,
+        startDate: dateRange.startDate,
+        endDate: dateRange.endDate,
+        listingId: listing?.id
+      })
+      .then(() => {
+        toast.success('Listing reserved!');
+        // redirect to trips
+        router.refresh()
+      })
+      .catch(() => {
+        toast.error('Something went wrong.');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  },
+  [
+    totalPrice, 
+    dateRange, 
+    listing?.id,
+    router,
+    currentUser,
+    loginModal
+  ]);
+
 
 
 
