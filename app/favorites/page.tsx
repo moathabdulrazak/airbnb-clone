9259d7d0a4366b2bbbf43f4f5d1ab21021a670d0
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 08:42:44 -0600

    getting my favorites.

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -4,16 +4,32 @@
 import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
+import FavoritesClient from "./FavoritesClient";
 
 const ListingPage = async () => {
+const listings = await getFavoriteListings();
+const currentUser = await getFavoriteListings();
+
+if(listings.length === 0){
+  <ClientOnly>
+  <EmptyState
+  title="No favorites found"
+  subtitle="Looks like you have no favorite listings"
+  />
+</ClientOnly>
+}
+
   return (
     <ClientOnly>
-      <EmptyState
-      title="No favorites found"
-      subtitle="Looks like you have no favorite listings"
+      <FavoritesClient
+      listings={listings}
+      currentUser={currentUser}
       />
     </ClientOnly>
+
   )
+
 }
 
 
