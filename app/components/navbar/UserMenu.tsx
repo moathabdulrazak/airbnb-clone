fd046095929f420dd259c6a10a2b2262b595ea14
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 08:01:20 -0600

    reservation page in works

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -107,7 +107,7 @@ if(!currentUser){
           label='My favorites'
           />
           <MenuItem
-          onClick={() => {}}
+          onClick={() => router.push('/reservations')}
           label='My reservations'
           />
           <MenuItem
--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -4,10 +4,46 @@ import ClientOnly from "../components/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
 
 import getReservations from "../actions/getReservations";
+import ReservationsClient from "./ReservationsClient";
 
 
 const ReservationPage = async () => {
 const currentUser = await getCurrentUser();
+
+
+if(!currentUser){
+  <ClientOnly>
+    <EmptyState
+    title="Unauthorized"
+    subtitle="Please Login"
+    />
+  </ClientOnly>
+}
+
+const reservations = await getReservations({
+  authorId: currentUser?.id
+})
+
+if(reservations.length === 0){
+  return (
+    <ClientOnly>
+      <EmptyState
+      title="No reservations found"
+      subtitle="Looks like you have reservations on your properties"
+      />
+    </ClientOnly>
+  )
+}
+
+return(
+  <ClientOnly>
+    <ReservationsClient
+    reservations={reservations}
+    currentUser = {currentUser}
+    />
+  </ClientOnly>
+)
+
 }
 
 
