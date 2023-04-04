c77aa4021ebb5303390daa957f828e1dd00c79ad
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 04:04:12 -0600

    Calander added

---
+++ app/components/inputs/Calendar.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+
+import React from 'react'
+import { DateRange, Range, RangeKeyDict } from 'react-date-range'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+
+interface CalendarProps{
+  value: Range
+  onChange: (value: RangeKeyDict) => void
+  disabledDates?: Date[]
+}
+
+const Calendar: React.FC<CalendarProps> = ({
+  value,
+  onChange,
+  disabledDates
+}) => {
+  return (
+    <DateRange
+    
+    />
+  )
+}
+
+export default Calendar
--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -3,6 +3,7 @@
 
 import React from 'react'
 import {Range} from 'react-date-range'
+import Calendar from '../inputs/Calendar';
 
 interface ListingReservationProps{
   price: number
@@ -25,7 +26,23 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
   disabledDates
 }) => {
   return (
-    <div>ListingReservation</div>
+    <div className='bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden ' >
+
+      <div className='flex flex-row items-center gap-1 p-4' >
+        <div className='text-2xl font-semibold' >
+            $ {price}
+        </div>
+        <div className='font-light text-neutral-600' >
+          night
+        </div>
+      </div>
+      <hr />
+      <Calendar
+      value={dateRange}
+      disabledDates={disabledDates}
+      onChange={(value) => onChangeDate(value.selection) }
+      />
+    </div>
   )
 }
 
