d450ccd805ef14eefecc162b275dd1058296969c
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 12:02:28 -0600

    UPDATED README

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -10,17 +10,14 @@ const Logo = () => {
   const router = useRouter()
 
   return (
-    // <Image 
-    // onClick={() => router.push('/')}
-    // alt="Logo"
-    // className="hidden md:block cursor-pointer"
-    // height={100}
-    // width={100}
-    // src={'/images/logo.png'}
-    // /> 
-    <div>
-      <h1>Airbnb</h1>
-    </div>
+    <Image 
+    onClick={() => router.push('/')}
+    alt="Logo"
+    className="hidden md:block cursor-pointer"
+    height={100}
+    width={100}
+    src={'/images/logo.png'}
+    /> 
   )
 }
 
