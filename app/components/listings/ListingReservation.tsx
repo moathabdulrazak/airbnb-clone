a2a4da3dbcb624b00d1dde15b166fa1e23bd2ba2
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 04:12:58 -0600

    reserve button added.

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -4,6 +4,7 @@
 import React from 'react'
 import {Range} from 'react-date-range'
 import Calendar from '../inputs/Calendar';
+import Button from '../Button';
 
 interface ListingReservationProps{
   price: number
@@ -43,6 +44,13 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
       onChange={(value) => onChangeDate(value.selection) }
       />
       <hr />
+      <div className='p-4' >
+        <Button
+        disabled={disabled}
+        label='Reserve'
+        onClick={onSubmit}
+        />
+      </div>
       <div className='p-4 flex flex-row items-center justify-between font-semibold text-lg' >
         <div>
           Total
