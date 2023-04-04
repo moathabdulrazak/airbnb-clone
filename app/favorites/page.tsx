0912e63c4cfaee1c1fa122e6353029db9646756f
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 08:45:05 -0600

    small fixes.

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,36 +1,34 @@
+import EmptyState from "@/app/components/EmptyState";
+import ClientOnly from "@/app/components/ClientOnly";
 
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getFavoriteListings from "@/app/actions/getFavoriteListings";
 
-
-import EmptyState from "../components/EmptyState";
-import ClientOnly from "../components/ClientOnly";
-import getCurrentUser from "../actions/getCurrentUser";
-import getFavoriteListings from "../actions/getFavoriteListings";
 import FavoritesClient from "./FavoritesClient";
 
 const ListingPage = async () => {
-const listings = await getFavoriteListings();
-const currentUser = await getFavoriteListings();
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
 
-if(listings.length === 0){
-  <ClientOnly>
-  <EmptyState
-  title="No favorites found"
-  subtitle="Looks like you have no favorite listings"
-  />
-</ClientOnly>
-}
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings."
+        />
+      </ClientOnly>
+    );
+  }
 
   return (
     <ClientOnly>
       <FavoritesClient
-      listings={listings}
-      currentUser={currentUser}
+        listings={listings}
+        currentUser={currentUser}
       />
     </ClientOnly>
-
-  )
-
+  );
 }
-
-
+ 
 export default ListingPage;
