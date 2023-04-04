fb0495f8812b7921117c56bc6ff5a7ebc22a3316
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 07:28:08 -0600

    trips page, you can see your reservations

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -1,13 +1,70 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { useCallback, useState } from 'react'
+import { SafeReservation, SafeUser } from '../types'
+import Container from '../components/Container';
+import Heading from '../components/Heading';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import ListingCard from '../components/listings/ListingCard';
+
+interface TripsClientProps{
+  reservations: SafeReservation[];
+  currentUser?: SafeUser | null
+
+}
 
 
-import React from 'react'
+const TripsClient: React.FC<TripsClientProps> = ({
+  reservations,
+  currentUser
+}) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState('')
 
 
 
 
-const TripsClient = () => {
+  const onCancel = useCallback((id: string) => {
+      setDeletingId(id);
+
+      axios.delete(`/api/reservations/${id}`)
+      .then(() => {
+        toast.success('Reservation canceled')
+        router.refresh();
+      })
+      .catch((error) => {
+        toast.error(error?.response?.data?.error)
+      })
+      .finally(() => {
+        setDeletingId('')
+      })
+  }, [router])
+
+
+
   return (
-    <div>TripsClient</div>
+    <Container>
+      <Heading
+      center
+      title='Trips'
+      subtile="Where you've been and where you're going!"
+      />
+      <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6' >
+        {reservations.map((reservation) => (
+          <ListingCard
+          key={reservation.id}
+          data={reservation.listing}
+          reservation={reservation}
+          actionId={reservation.id}
+          onAction={onCancel}
+          disabled={deletingId === reservation.id}
+          actionLabel='Cancel reservation'
+          currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
   )
 }
 
