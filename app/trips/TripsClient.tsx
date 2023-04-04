ae2681d5be63da2745d6fe7bfc915b6e4af51dd9
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 07:30:51 -0600

    small fixes/

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -45,26 +45,37 @@ const TripsClient: React.FC<TripsClientProps> = ({
 
   return (
     <Container>
-      <Heading
-      center
-      title='Trips'
-      subtile="Where you've been and where you're going!"
-      />
-      <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6' >
-        {reservations.map((reservation) => (
-          <ListingCard
+    <Heading
+      title="Trips"
+      subtile="Where you've been and where you're going"
+    />
+    <div 
+      className="
+        mt-10
+        grid 
+        grid-cols-1 
+        sm:grid-cols-2 
+        md:grid-cols-3 
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-8
+      "
+    >
+      {reservations.map((reservation: any) => (
+        <ListingCard
           key={reservation.id}
           data={reservation.listing}
           reservation={reservation}
           actionId={reservation.id}
           onAction={onCancel}
           disabled={deletingId === reservation.id}
-          actionLabel='Cancel reservation'
+          actionLabel="Cancel reservation"
           currentUser={currentUser}
-          />
-        ))}
-      </div>
-    </Container>
+        />
+      ))}
+    </div>
+  </Container>
   )
 }
 
