62362d57fe6c77a83bd27baea3f0f0b3aa5dea68
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 07:12:38 -0600

    you can go trips page.

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from '@/app/hooks/useLoginModal'
 import { signOut } from 'next-auth/react'
 import { SafeUser } from '@/app/types'
 import useRentModal from '@/app/hooks/useRentModal'
+import { useRouter } from 'next/navigation'
 
 interface UserMenuProps{
 currentUser?: SafeUser | null
@@ -17,6 +18,7 @@ currentUser?: SafeUser | null
 export const UserMenu: React.FC<UserMenuProps> = ({
   currentUser,
 }) => {
+  const router = useRouter();
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
@@ -97,7 +99,7 @@ if(!currentUser){
             {currentUser ? (
           <>
           <MenuItem
-          onClick={() => {}}
+          onClick={() => router.push('/trips')}
           label='My trips'
           />
           <MenuItem
---
+++ app/trips/TripsClient.tsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+
+
+
+const TripsClient = () => {
+  return (
+    <div>TripsClient</div>
+  )
+}
+
+export default TripsClient
