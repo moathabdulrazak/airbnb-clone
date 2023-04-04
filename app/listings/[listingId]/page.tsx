c254d2b77423f62ca768ae275c69f99ceeb8e5b9
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-04-04 07:00:26 -0600

    safe resveration type created

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -4,6 +4,7 @@ import ClientOnly from '@/app/components/ClientOnly';
 import EmptyState from '@/app/components/EmptyState';
 import React from 'react'
 import ListingClient from './ListingClient';
+import getReservations from '@/app/actions/getReservations';
 
 interface IParms {
   listingId?: string;
@@ -11,6 +12,7 @@ interface IParms {
 
 const listingPage = async ({ params }: { params: IParms }) => {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
   
   if(!listing){
@@ -25,6 +27,7 @@ const listingPage = async ({ params }: { params: IParms }) => {
     <ClientOnly>
       <ListingClient
       listing={listing}
+      reservations={reservations}
       currentUser={currentUser}
       />
     </ClientOnly>
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
-import { Listing, User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
 
 
 
@@ -6,6 +6,16 @@ export type SafeListing = Omit<Listing, "createdAt"> & {
   createdAt: string;
 };
 
+
+export type SafeReservation = Omit<
+Reservation,
+"createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string
+  startDate: string
+  endDate: string
+  listing: SafeListing
+}
 
 export type SafeUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified" > & {
   createdAt: string
